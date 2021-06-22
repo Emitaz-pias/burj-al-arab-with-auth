d2afa1fe82739a1596ca9af4681a32a6f36d0cd6
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-22 19:34:51 +0600

    google pop up sign in added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuftiP2I6ZngwT1OnffHhNAEM18UE3OiY",
+  authDomain: "burj-al-arab-f4353.firebaseapp.com",
+  projectId: "burj-al-arab-f4353",
+  storageBucket: "burj-al-arab-f4353.appspot.com",
+  messagingSenderId: "709465954386",
+  appId: "1:709465954386:web:d148f658ebdb13784f278b",
+};
