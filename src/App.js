135f5cff11b35a4b7b12f5a7cb411d88b905c515
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-23 10:38:49 +0600

    state updated from login page and used to header component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Book from "./components/Book/Book";
 import Header from "./components/Header/Header";
-
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 export const UserContext = createContext();
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState({ name: "pias" });
+  const [loggedInUser, setLoggedInUser] = useState({});
 
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
@@ -22,9 +22,12 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/book/:bedType">
+          <PrivateRoute path="/book/:bedType">
             <Book />
-          </Route>
+          </PrivateRoute>
+          <PrivateRoute path="/book">
+            <Book />
+          </PrivateRoute>
           <Route exact path="/">
             <Home />
           </Route>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,34 +2,47 @@ import React, { useContext } from "react";
 import { UserContext } from "../../App";
 import { firebaseConfig } from "./firebase.config";
 import firebase from "firebase/app";
-
+import "firebase/auth";
+import {
+  BrowserRouter as Router,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 firebase.initializeApp(firebaseConfig);
-const provider = new firebase.auth.GoogleAuthProvider();
-
 const Login = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      // const  credential = result.credential;
-      // var token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      setLoggedInUser(user);
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
+  const history = useHistory();
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
 
-      const errorMessage = error.message;
-      console.log(errorMessage);
-    });
+  const handleClick = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        // const  credential = result.credential;
+        // var token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        const userInfo = { name: user.displayName, email: user.email };
+        setLoggedInUser(userInfo);
+        history.replace(from);
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
 
   return (
     <div>
       <h1>This is Login</h1>
+      <button onClick={handleClick}>Continue with google</button>
     </div>
   );
 };
